4610cf665bbc07c3baa82bbe5c6bf935a7407fac
Staszek <stanislaw.polanski@hotmail.com>
2018-06-22 21:41:22 +0200

    Districts table designed

---
+++ FootballSchedulerDB/Tables/Districts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Districts]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [ParentId] INT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [Name_Unique] ON [dbo].[Districts] ([Name])
