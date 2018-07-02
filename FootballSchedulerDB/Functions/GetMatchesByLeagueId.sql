432d8d4320bce156e5ce681926907e7bb6b5d5da
Staszek <stanislaw.polanski@hotmail.com>
2018-07-02 21:54:19 +0200

    View and function finished

---
+++ FootballSchedulerDB/Functions/GetMatchesByLeagueId.sql
@@ -0,0 +1,21 @@
+﻿CREATE FUNCTION [dbo].[GetMatchesByLeagueId]
+(
+	@LeagueId int
+)
+RETURNS @returntable TABLE
+(
+	TimeOfPlay datetime,
+	HomeTeamId int,
+	AwayTeamId int,
+	HomeScore int,
+	AwayScore int
+)
+AS
+BEGIN
+	INSERT @returntable
+	SELECT
+		TimeOfPlay, HomeTeamId, AwayTeamId, HomeScore, AwayScore
+		from dbo.Matches
+		where dbo.Matches.LeagueId = @LeagueId
+	RETURN
+END
---
+++ FootballSchedulerDB/Views/GetLeaguePlayYears.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [dbo].[GetLeaguePlayYears]
+	as select year(StartDate) as YearsOfPlay from dbo.Leagues group by StartDate
